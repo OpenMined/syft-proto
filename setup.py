8fd96e61c19e6a6336f12c21b0e68f7f4377f4f1
Karl Higley <kmhigley@gmail.com>
2019-12-28 10:52:15 -0500

    Use `find_packages()` in `setup.py` to include all sub-directories

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="PySyft protocol constants.",
-    packages=["syft_proto"],
+    packages=find_packages(),
     package_data={"": ["proto.json"]},
     install_requires=["protobuf>=3.11.1"],
     license="LICENSE",
