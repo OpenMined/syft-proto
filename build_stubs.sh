b7ab792f5980aef94171e3f5f31f93421a1bb6f6
Karl Higley <kmhigley@gmail.com>
2019-12-28 13:24:46 -0500

    Update the Protobuf stub building script to create proper Python pkgs
    It's kinda silly that the Protobuf compiler doesn't do this when it
    outputs Python stubs, but it's not too hard to fix with some script
    tweaks.

--- build_stubs.sh
+++ build_stubs.sh
@@ -1,3 +1,8 @@
+# Build stub from Protobuf schemas
 cd protobuf
 buf check lint && buf image build -o - | protoc --descriptor_set_in=/dev/stdin --python_out=../ $(buf image build -o - | buf ls-files --input -)
-cd - > /dev/null
+cd ..
+
+# Create Python sub-packages for the stubs
+find syft_proto/ -type d -print0 |
+  while IFS= read -rd '' dir; do touch "$dir/__init__.py"; done
